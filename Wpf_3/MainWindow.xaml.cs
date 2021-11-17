b2827a329860d8686a8334b2ca800f7e93eaf480
Сергей Куцев <Sergeikutsev34@gmail.com>
2021-11-17 17:44:50 +0300

    Добавьте файлы проекта.

---
+++ Wpf_3/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_8
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName=(sender as ComboBox).SelectedItem as string;
+            if (textBox!=null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+            
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            double Fontsize = Convert.ToDouble((sender as ComboBox).SelectedItem as string);
+
+            if (textBox != null)
+            {
+                textBox.FontSize = Fontsize;
+            }
+
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(textBox.FontWeight == FontWeights.Normal)
+            textBox.FontWeight = FontWeights.Bold;
+             else
+                textBox.FontWeight =FontWeights.Normal;
+        }
+
+        private void Image_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            if(textBox.FontStyle == FontStyles.Normal)
+            textBox.FontStyle=FontStyles.Italic;
+            else
+                textBox.FontStyle = FontStyles.Normal;
+        }
+
+        private void Image_MouseLeftButtonDown_2(object sender, MouseButtonEventArgs e)
+        {
+            if (textBox.TextDecorations != TextDecorations.Underline)
+                textBox.TextDecorations = TextDecorations.Underline;
+            else
+                textBox.TextDecorations = null;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+                textBox.Foreground = Brushes.Black;
+        }
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+                textBox.Foreground = Brushes.Red;
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFile.ShowDialog() == true)
+            {
+
+                textBox.Text = File.ReadAllText(openFile.FileName);
+            }
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ExitCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (textBox != null && textBox.Text.Length == 0)
+            { e.CanExecute = true; }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+    }
+}
